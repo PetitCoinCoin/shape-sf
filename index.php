148cdb97219bb73c61fbcc248356773f48ef01aa
Andréa <andrea.marnier@gmail.com>
2025-07-27 13:56:17 +0200

    feat: index base

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<!--
+	Arcana by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Shape Saddle-fitting</title>
+		<?php include("pages/commons/meta.html"); ?>
+	</head>
+
+	<body class="is-preload">
+		<div id="page-wrapper">
+			
+			<!-- Header -->
+			<?php include("pages/commons/header.html"); ?>
+
+			<!-- Footer -->
+			<?php include("pages/commons/footer.html"); ?>
+
+		</div>
+
+		<!-- Scripts -->
+		<?php include("pages/commons/scripts.html"); ?>
+		<script>$('#nav-home').addClass('current')</script>
+	</body>
+</html>
