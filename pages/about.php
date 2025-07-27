d4893d7d52744a6eb25d84d9c7280d3606b5d327
Andréa <andrea.marnier@gmail.com>
2025-07-27 18:25:31 +0200

    feat: about page
    add to about

---
+++ pages/about.php
@@ -0,0 +1,95 @@
+<!DOCTYPE HTML>
+<!--
+	Arcana by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Shape Saddle-fitting - À propos</title>
+		<?php include("commons/meta.html"); ?>
+		<link rel="stylesheet" href="/assets/css/main.css" />
+	</head>
+
+	<body class="is-preload">
+		<div id="page-wrapper">
+			
+			<!-- Header -->
+			<?php include("commons/header.html"); ?>
+			<!-- Main -->
+				<section class="wrapper style1">
+					<div class="container">
+						<div id="content">
+							<img id="me-img" height="120px" src="../assets/images/me.jpg" alt="me" />
+							<p>
+								En 2010, j’ai enfin réalisé mon rêve de devenir propriétaire de mon premier cheval: 
+								Shining, un pur-sang anglais réformé des courses.
+							</p>
+							<p>
+								Quel étrange animal, sur lequel je voyais bien qu’aucune selle n’allait: elles touchaient le garrot, 
+								ne tombaient pas bien. J’ai bien entendu eu les fameux « Mais si, c’est une selle XXX, elle va à tous 
+								les chevaux » (par quelle étrange magie noire ?) ou « Mets un pad de garrot » (VadeRetroSatanas pour 
+								cet object qui ne devrait pas exister).
+							</p>
+							<p>
+								D’un autre côté, ma formation initiale d’ingénieure en gestion d’aéroport, et une forte propension à la 
+								logistique et à l’optimisation des flux, me poussent à comprendre en profondeur les choses: observer, analyser, 
+								optimiser. Les processus au travail. Mon cheval à côté. Et du coup, j’ai lu, confronté, cherché. J'ai d'ailleurs 
+								beaucoup lu en anglais, ces personnes ayant gardé l’excellent réflexe d’adapter les selles aux diverses 
+								morphologies de chevaux présents sur leur île, et savent seller ces étranges pur-sangs qui viennent de chez eux. 
+								J’ai fait venir des Saddle-Fitteurs. Appris des choses. Acheté et revendu des selles (pour de bonnes ou de 
+								mauvaises raisons). Et j'ai compris la difficulté du sujet.
+							</p>
+							<p>
+								En 2020, j’ai décidé de me former auprès d’Ergonomie Equestre (pour le côté holistique par lequel cette formation 
+								aborde le saddle-fitting), et une formidable opportunité s’est présentée à moi pour en faire une activité professionnelle.
+							</p>
+							<p>
+								Depuis, je sillonne donc les routes de la région Toulousaine avec cette mission: aider les couples équidé/humain à 
+								trouver cet objet, ô combien méconnu des cavaliers: la selle qui leur convient à l’instant T.
+							</p>
+							<p>
+								Cavalière de Complet, mais surtout en recherche d’une pratique polyvalente sportive avec un cheval Partenaire 
+								(Hunter, CSO, dressage, TREC, Equifeel, Mountain Trail), je saurai écouter cavaliers de loisirs et de sport pour 
+								trouver LA selle qui convient à votre pratique actuelle et/ou vos objectifs. 
+							</p>
+						</div>
+						<div class="row gtr-200">
+							<div class="col-4 col-12-narrower">
+								<figure role="figure" class="image featured" aria-label="Shining, pur-sang anglais">
+									<img src="../assets/images/about01.jpg" alt="a horse" />
+									<figcaption>
+										Shining King, réformé en 2010, à l’origine de de cette reconversion
+									</figcaption>
+								</figure>
+							</div>
+							<div class="col-4 col-12-narrower">
+								<figure role="figure" class="image featured" aria-label="Un cheval">
+									<img src="../assets/images/about02.jpg" alt="a woman and a horse" />
+									<figcaption>
+										En compétition, Hunter Club Elite, Avril 2019
+									</figcaption>
+								</figure>
+							</div>
+							<div class="col-4 col-12-narrower">
+								<figure role="figure" class="image featured" aria-label="Un cheval">
+									<img src="../assets/images/about03.jpg" alt="a woman and a horse" />
+									<figcaption>
+										Entrainement Mountain Trail, Juillet 2019
+									</figcaption>
+								</figure>
+							</div>
+						</div>
+					</div>
+				</section>
+
+			<!-- Footer -->
+			<?php include("commons/footer.html"); ?>
+
+		</div>
+
+		<!-- Scripts -->
+		<?php include("commons/scripts.html"); ?>
+		<script>$('#nav-about').addClass('current')</script>
+	</body>
+</html>
