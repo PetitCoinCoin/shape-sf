57ee27d5cffb66282e501cf0b77d5a75b814256f
Andréa <andrea.marnier@gmail.com>
2025-07-27 18:55:27 +0200

    feat: contact page

---
+++ pages/contact.php
@@ -0,0 +1,56 @@
+<!DOCTYPE HTML>
+<!--
+	Arcana by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Shape Saddle-fitting - Contact</title>
+		<?php include("commons/meta.html"); ?>
+		<link rel="stylesheet" href="/assets/css/main.css" />
+	</head>
+
+	<body class="is-preload">	
+		<div id="page-wrapper">
+			
+			<!-- Header -->
+			<?php include("commons/header.html"); ?>
+			<!-- Main -->
+				<section class="wrapper style1">
+					<div class="container">
+						<div class="row gtr-200">
+							<div class="col-4 col-12-narrower">
+								<figure role="figure" class="image featured" aria-label="Un cheval au galop">
+									<img src="/assets/images/contact.jpg" alt="a horse running" />
+									<figcaption>
+										Photo by <a href="https://unsplash.com/@kimberlyarchambaultphoto?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Kimberly Archambault</a> on <a href="https://unsplash.com/photos/a-horse-running-on-a-dirt-track-JSJTEzgNks8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+									</figcaption>
+								</figure>
+							</div>
+							<div class="col-8 col-12-narrower">
+								<div id="content">
+									<h3>Morgane Pellegrinelli</h3>
+									<h4>Conseil en ergonomie de la selle</h4>
+									<p>
+										<span><b>Email:</b> m.p.ergo.selle@gmail.com</span><br/>
+										<span><b>Téléphone:</b> 07 88 11 95 46</span><br />
+										<span><b>Facebook:</b>  https://www.facebook.com/profile.php?id=100063739141304</span>
+									</p>
+									<p>Merci de privilégier les emails afin de ne pas éparpiller les sollicitations !</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+			<!-- Footer -->
+			<?php include("commons/footer.html"); ?>
+
+		</div>
+
+		<!-- Scripts -->
+		<?php include("commons/scripts.html"); ?>
+		<script>$('#nav-contact').addClass('current')</script>
+	</body>
+</html>
