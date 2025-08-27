2ba16bfb71b03886f8c6928c7a65003bdea5f936
Andréa <andrea.marnier@gmail.com>
2025-08-27 22:22:39 +0200

    feat: update about page

--- pages/about.php
+++ pages/about.php
@@ -21,18 +21,29 @@
 			<!-- Main -->
 				<section class="wrapper style1">
 					<div class="container">
+						<div class="row gtr-200">
+							<div class="col-4 col-12-narrower">
+								<figure role="figure" class="image featured" aria-label="Une femme et un cheval dans un champ">
+									<img src="../assets/images/about01.jpg" alt="Une femme et un cheval dans un champ" />
+								</figure>
+							</div>
+							<div class="col-8 col-12-narrower children-vertical">
+								<div id="content">
+									<h3>Qui suis-je ?</h3>
+									<p>
+										En 2010, j’ai enfin réalisé mon rêve de devenir propriétaire de mon premier cheval: 
+										Shining, un pur-sang anglais réformé des courses.
+									</p>
+									<p>
+										Quel étrange animal, sur lequel je voyais bien qu’aucune selle n’allait: elles touchaient le garrot, 
+										ne tombaient pas bien. J’ai bien entendu eu les fameux « Mais si, c’est une selle XXX, elle va à tous 
+										les chevaux » (par quelle étrange magie noire ?) ou « Mets un pad de garrot » (Vade Retro Satanas pour 
+										cet objet qui ne devrait pas exister).
+									</p>
+								</div>
+							</div>
+						</div>
 						<div id="content">
-							<img id="me-img" height="120px" src="../assets/images/me.jpg" alt="me" />
-							<p>
-								En 2010, j’ai enfin réalisé mon rêve de devenir propriétaire de mon premier cheval: 
-								Shining, un pur-sang anglais réformé des courses.
-							</p>
-							<p>
-								Quel étrange animal, sur lequel je voyais bien qu’aucune selle n’allait: elles touchaient le garrot, 
-								ne tombaient pas bien. J’ai bien entendu eu les fameux « Mais si, c’est une selle XXX, elle va à tous 
-								les chevaux » (par quelle étrange magie noire ?) ou « Mets un pad de garrot » (VadeRetroSatanas pour 
-								cet object qui ne devrait pas exister).
-							</p>
 							<p>
 								D’un autre côté, ma formation initiale d’ingénieure en gestion d’aéroport, et une forte propension à la 
 								logistique et à l’optimisation des flux, me poussent à comprendre en profondeur les choses: observer, analyser, 
@@ -40,47 +51,46 @@
 								beaucoup lu en anglais, ces personnes ayant gardé l’excellent réflexe d’adapter les selles aux diverses 
 								morphologies de chevaux présents sur leur île, et savent seller ces étranges pur-sangs qui viennent de chez eux. 
 								J’ai fait venir des Saddle-Fitteurs. Appris des choses. Acheté et revendu des selles (pour de bonnes ou de 
-								mauvaises raisons). Et j'ai compris la difficulté du sujet.
+								mauvaises raisons). Et j'ai compris la difficulté du sujet aux multiples paramètres.
 							</p>
+							<figure role="figure" class="image divider" aria-label="Une femme sur un cheval sautant un obstacle">
+								<img src="../assets/images/about02.jpg" alt="Une femme sur un cheval sautant un obstacle" />
+							</figure>
 							<p>
 								En 2020, j’ai décidé de me former auprès d’Ergonomie Equestre (pour le côté holistique par lequel cette formation 
-								aborde le saddle-fitting), et une formidable opportunité s’est présentée à moi pour en faire une activité professionnelle.
+								aborde le saddle-fitting), et une formidable opportunité s’est présentée à moi pour en faire une activité professionnelle. 
+								Depuis, je sillonne donc les routes de la région Toulousaine avec cette mission: aider les couples équidé/humain à trouver 
+								cet objet, ô combien méconnu des cavaliers: la selle qui leur convient, et qui pourra s’adapter !
 							</p>
 							<p>
-								Depuis, je sillonne donc les routes de la région Toulousaine avec cette mission: aider les couples équidé/humain à 
-								trouver cet objet, ô combien méconnu des cavaliers: la selle qui leur convient à l’instant T.
+								Shining m’a quitté une fois son méfait accompli (ma reconversion professionnelle actée), et Ivy s’est présenté à moi de 
+								façon évidente. Lui m’accompagne dans ma recherche personnelle d’une équitation de sport discrète et neutre. Il me permet 
+								également d’expérimenter toute la préparation physique idéale d’un cheval destiné à porter un cavalier sainement. 
+							</p>
+							<figure role="figure" class="image divider" aria-label="Une femme caressant un cheval">
+								<img src="../assets/images/about03.jpg" alt="Une femme caressant un cheval" />
+							</figure>
+							<p>
+								Cavalière de Complet et de Hunter, mais surtout en recherche d’une pratique polyvalente sportive avec un cheval-partenaire 
+								(CSO, dressage, TREC, Equifeel, Mountain Trail, extérieur), je saurai écouter cavaliers de loisirs et de sport pour trouver 
+								LA selle qui convient à votre pratique actuelle et/ou vos objectifs.
 							</p>
 							<p>
-								Cavalière de Complet, mais surtout en recherche d’une pratique polyvalente sportive avec un cheval Partenaire 
-								(Hunter, CSO, dressage, TREC, Equifeel, Mountain Trail), je saurai écouter cavaliers de loisirs et de sport pour 
-								trouver LA selle qui convient à votre pratique actuelle et/ou vos objectifs. 
+								<h6>Formations:</h6>
+								<ul>
+									<li>2006: diplôme ENSIACET Ingénieure en Génie Chimique</li> 
+									<li>2007: Master Spécialisé ENAC Management des Aéroports</li>
+									<li>2020: Formation en ergonomie du matériel (Ergonomie Equestre)</li>
+									<li>2021: Initiation Equitation Centrée</li>
+									<li>2024: Formation Complémentaire flocage Stubben</li>
+									<li>2024: Formation Ergonomie du bridon et des embouchures Ergo Equine</li>
+									<li>2025: Formations Préférences Motrices (Axel Sport) et Lecture de posture du Cavalier (Sandra Fabritz)</li>
+								</ul>
+								Formations continues en biomécanique & locomotion et parage/équilibrage du pied.
 							</p>
-						</div>
-						<div class="row gtr-200">
-							<div class="col-4 col-12-narrower">
-								<figure role="figure" class="image featured" aria-label="Shining, pur-sang anglais">
-									<img src="../assets/images/about01.jpg" alt="a horse" />
-									<figcaption>
-										Shining King, réformé en 2010, à l’origine de de cette reconversion
-									</figcaption>
-								</figure>
-							</div>
-							<div class="col-4 col-12-narrower">
-								<figure role="figure" class="image featured" aria-label="Un cheval">
-									<img src="../assets/images/about02.jpg" alt="a woman and a horse" />
-									<figcaption>
-										En compétition, Hunter Club Elite, Avril 2019
-									</figcaption>
-								</figure>
-							</div>
-							<div class="col-4 col-12-narrower">
-								<figure role="figure" class="image featured" aria-label="Un cheval">
-									<img src="../assets/images/about03.jpg" alt="a woman and a horse" />
-									<figcaption>
-										Entrainement Mountain Trail, Juillet 2019
-									</figcaption>
-								</figure>
-							</div>
+							<figure role="figure" class="image divider" aria-label="Une femme sur un cheval sautant un obstacle">
+								<img src="../assets/images/about04.jpg" alt="Une femme sur un cheval sautant un obstacle" />
+							</figure>
 						</div>
 					</div>
 				</section>
