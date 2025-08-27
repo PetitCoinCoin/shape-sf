3b6788844b02ffb3994977596328d0d0a75a4c16
Andréa <andrea.marnier@gmail.com>
2025-08-27 21:32:22 +0200

    feat: update home page

--- index.php
+++ index.php
@@ -22,31 +22,42 @@
 				<section class="wrapper style1">
 					<div class="container">
 						<div class="row gtr-200">
+							<div class="col-8 col-12-narrower">
+								<div id="content">
+									<p>
+										Votre cheval manifeste des signes d’inconfort ou des défenses au sanglage ?<br />
+										Il bouge quand il voit la selle ?<br />
+										Ses allures sont étriquées sous la selle, mais développées en longe ?<br />
+										Son attitude est instable ?<br />
+										Votre enseignant vous répète en permanence de tenir votre dos, ou de reculer vos jambes?
+									</p>
+									<p class="light-emphasis">
+										Autant de signes qui pointent vers une selle inadaptée, au cheval et/ou au cavalier.
+									</p>
+									<p>
+										La culture générale du cavalier français, et le marché francais de la sellerie quasiment uniquement constitué de selles non modifiables (et fort peu de vrai sur-mesure, quand on se penche sur la question), occulte cette partie ô combien essentielle pour le cavalier et son cheval.
+									</p>
+								</div>
+							</div>
 							<div class="col-4 col-12-narrower">
-								<figure role="figure" class="image featured" aria-label="Une selle">
-									<img src="assets/images/home.jpg" alt="saddle" />
-									<figcaption>
-										Photo by <a href="https://unsplash.com/@photomatic_s?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Matias Tapia</a> on <a href="https://unsplash.com/photos/black-leather-backpack-with-black-leather-belt-juZ8bACGT6U?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-									</figcaption>
+								<figure role="figure" class="image featured" aria-label="Une femme sellant un cheval">
+									<img src="assets/images/home01.jpg" alt="Une femme sellant un cheval" />
+								</figure>
+							</div>
+						</div>
+						<div class="row gtr-200">
+							<div class="col-4 col-12-narrower">
+								<figure role="figure" class="image featured" aria-label="Des selles">
+									<img src="assets/images/home02.jpg" alt="Des selles" />
 								</figure>
 							</div>
 							<div class="col-8 col-12-narrower">
 								<div id="content">
 									<p>
-										Votre cheval manifeste des signes d’inconfort au sanglage ?<br />
-										Il bouge quand il voit la selle ?<br />
-										Ses allures sont étriquées sous la selle, mais développées en longe ?<br />
-										Votre enseignant vous répète en permanence de tenir votre dos, ou de reculer vos jambes?
-									</p>
-									<p class="light-emphasis">
-										Autant de signes qui pointent vers une selle inadaptée.
-									</p>
-									<p>
-										La culture générale du cavalier français, et le marché francais de la sellerie quasiment uniquement constitué de selles non modifiables (et fort peu de vrai sur-mesure, quand on se penche sur la question), occulte cette partie ô combien essentielle pour le cavalier et son cheval.</p>
-									<p>
 										Faire vérifier sa selle, ou choisir une selle via le conseil d’un « saddle-fitter », c’est offrir une selle 
 										qui n’occasionne pas de points de pression douloureux pour votre cheval, en jouant sur les tailles d’arcades, 
-										les formes d’arçons, les réglages d’équilibre de la selle, mais aussi en tenant compte de la sangle. Cela permet   
+										les formes d’arçons, les réglages d’équilibre de la selle, mais aussi en tenant compte de la sangle. Cela permet 
+
 										alors à votre cheval de developper sa vraie locomotion sans gêne.<br />
 										Mais c’est aussi vous offrir une selle contre laquelle vous ne vous battez pas, une selle qui vous verticalise 
 										sans efforts, et qui est adaptée à votre morphologie.
