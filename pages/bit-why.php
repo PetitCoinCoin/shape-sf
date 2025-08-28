45ae70ace6a5faf99cf2bc408012f2b61fe5fb06
Andréa <andrea.marnier@gmail.com>
2025-08-28 22:03:31 +0200

    feat: add bit fitting why page

---
+++ pages/bit-why.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+<!--
+	Arcana by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+
+	Modified by @PetitCoinCoin
+-->
+<html>
+	<head>
+		<title>Shape Bit-fitting - Pourquoi ?</title>
+		<?php include("commons/meta.html"); ?>
+		<link rel="stylesheet" href="/assets/css/main.css" />
+	</head>
+
+	<body class="is-preload">
+		<div id="page-wrapper">
+			
+			<!-- Header -->
+			<?php include("commons/header.html"); ?>
+			<!-- Main -->
+				<section class="wrapper style1">
+					<div class="container">
+						<h3>Un bridon et un mors adaptés, pourquoi ?</h3>
+						<div class="row gtr-200">
+							<div class="col-8 col-12-narrower">
+								<div class="content">
+									<p>
+										Contrairement aux idées communément répandues, un mors aux canons les plus épais possible 
+										n’est pas confortable pour le cheval. Le cintrage des canons, et leur diamètre/section sont 
+										le plus important. De plus, chaque cheval va avoir une préférence pour des formes de 
+										canons et des conceptions différentes en fonction de sa morphologie et de sa musculature, 
+										et proposer un contact très différent selon les embouchures.
+									</p>
+									<p>
+										L’ergonomie du mors influe ÉNORMEMENT sur la locomotion du cheval, car la langue est liée à 
+										l’ensemble du corps…
+									</p>
+									<p>
+										De même, la forme et la souplesse/rigidité de la têtière influent sur la souplesse de la nuque 
+										et l’ensemble de la locomotion, et donc vous allez pouvoir sentir en séance une facilité ou une 
+										réticence dans l’incurvation et/ou les mouvements latéraux. 
+									</p>
+								</div>
+							</div>
+							<div class="col-4 col-12-narrower">
+								<figure role="figure" class="image featured" aria-label="Des mors">
+									<img src="../assets/images/bitWhy01.jpg" alt="Des mors" />
+								</figure>
+							</div>
+						</div>
+					</div>
+				</section>
+
+			<!-- Footer -->
+			<?php include("commons/footer.html"); ?>
+
+		</div>
+
+		<!-- Scripts -->
+		<?php include("commons/scripts.html"); ?>
+		<script>$('#nav-bit-fitting').addClass('current')</script>
+	</body>
+</html>
