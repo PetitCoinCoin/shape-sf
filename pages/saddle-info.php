615acb185fe9e9a90de4f11d24c9c3cdf57ad1d3
Andréa <andrea.marnier@gmail.com>
2025-08-28 17:21:24 +0200

    feat: add saddle fitting info page

---
+++ pages/saddle-info.php
@@ -0,0 +1,94 @@
+<!DOCTYPE HTML>
+<!--
+	Arcana by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+
+	Modified by @PetitCoinCoin
+-->
+<html>
+	<head>
+		<title>Shape Saddle-fitting - Informations</title>
+		<?php include("commons/meta.html"); ?>
+		<link rel="stylesheet" href="/assets/css/main.css" />
+	</head>
+
+	<body class="is-preload">
+		<div id="page-wrapper">
+			
+			<!-- Header -->
+			<?php include("commons/header.html"); ?>
+			<!-- Main -->
+				<section class="wrapper style1">
+					<div class="container">
+						<h3>Informations sur la séance en ergonomie de la selle</h3>
+						<div class="row gtr-200">
+							<div class="col-8 col-12-narrower">
+								<div class="content">
+									<p>
+										Une séance en ergonomie de la selle, ce n’est pas juste mesurer l’arcade de la selle et trouver 
+										un modèle qui pose bien en statique. C’est pourquoi une séance dure 2h (dont environ 1h à cheval 
+										pour les essais).
+									</p>
+									<p>
+										Ceci me permet d’aborder les points suivants:
+										<ul>
+											<li>Anamnèse du couple (passif du cheval, du cavalier, etc)</li>
+											<li>Observation de la morphologie, de la posture et de la locomotion du cheval sans cavalier</li>
+											<li>Observation des morphologies & posture (à pied et en selle) du cavalier</li>
+											<li>Observation et discussion autour du matériel employé actuellement</li>
+											<li>Adéquation de la selle actuelle au dos du cheval en statique et en dynamique (monté)</li>
+											<li>Adaptation si possible de la selle (arcade & panneaux, correcteur d’équilibre, etc)</li>
+											<li>Adéquation de la selle au cavalier (essais de divers sièges et types d’arçons pour donner des sensations différentes)</li>
+    										<li>Essais de matériel annexe (étriers, sangles) pour l’optimisation de la locomotion du cheval et du fonctionnement du cavalier</li>
+										</ul>
+									</p>
+									<p>
+										Selon mes observations sur le couple, le contenu de la séance sera adapté (essais d’étriers pour centrer 
+										le cavalier avant d’essayer des sièges, réglages d’asymétries du couple, etc).
+									</p>
+								</div>
+							</div>
+							<div class="col-4 col-12-narrower">
+								<figure role="figure" class="image featured" aria-label="Des selles">
+									<img src="../assets/images/saddleInfo01.jpg" alt="Des selles" />
+								</figure>
+								<figure role="figure" class="image featured" aria-label="Des selles">
+									<img src="../assets/images/saddleInfo03.jpg" alt="Des selles" />
+								</figure>
+							</div>
+						</div>
+						<h3>Informations à connaitre avant la séance</h3>
+						<div class="row gtr-200">
+							<div class="col-4 col-12-narrower">
+								<figure role="figure" class="image featured" aria-label="Une femme tenant une selle">
+									<img src="../assets/images/saddleInfo02.jpg" alt="Une femme tenant une selle" />
+								</figure>
+							</div>
+							<div class="col-8 col-12-narrower">
+								<div class="content">
+									<p>Le cheval doit être pansé et prêt à seller pour l’heure du RDV, mais ni sellé ni échauffé au préalable.</p>
+									<p>
+										Vous devez être en tenue d’équitation et monter à cheval pendant la séance.</br>
+										Séance “pré-débourrage” possible, tarifs et durée moindres.
+									</p>
+									<p>
+										En amont de la séance, je vous enverrai un questionnaire à remplir afin de cerner votre besoin: 
+										votre couple, votre pratique et votre budget.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+			<!-- Footer -->
+			<?php include("commons/footer.html"); ?>
+
+		</div>
+
+		<!-- Scripts -->
+		<?php include("commons/scripts.html"); ?>
+		<script>$('#nav-saddle-fitting').addClass('current')</script>
+	</body>
+</html>
