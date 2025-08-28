3207bfaf134722f3615037f6351ba4112af1f39b
Andréa <andrea.marnier@gmail.com>
2025-08-28 18:18:28 +0200

    feat: add saddle fitting why page

---
+++ pages/saddle-why.php
@@ -0,0 +1,150 @@
+<!DOCTYPE HTML>
+<!--
+	Arcana by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+
+	Modified by @PetitCoinCoin
+-->
+<html>
+	<head>
+		<title>Shape Saddle-fitting - Pourquoi ?</title>
+		<?php include("commons/meta.html"); ?>
+		<link rel="stylesheet" href="/assets/css/main.css" />
+	</head>
+
+	<body class="is-preload">
+		<div id="page-wrapper">
+			
+			<!-- Header -->
+			<?php include("commons/header.html"); ?>
+			<!-- Main -->
+				<section class="wrapper style1">
+					<div class="container">
+						<h3>Une selle adaptable et adaptée, pourquoi ?</h3>
+						<div class="row gtr-200">
+							<div class="col-8 col-12-narrower">
+								<div class="content">
+									<p>
+										La tradition de sellerie française va à la selle non adaptable (ou tout du moins, 
+										pas autant qu’un saddle-fitter aime, et pas facilement): arçon traditionnel en bois 
+										et panneaux mousse.
+									</p>
+									<p>
+										Concernant le choix de l’arçon bois, on trouve du très très bon comme du très mauvais 
+										avec ce matériel, que ce soit en terme de qualité de transmission d’information cavalier/cheval, 
+										ou de qualité intrinsèque (sécurité/durabilité). Cet élément sera donc très variable selon les 
+										marques. De plus, ces arçons ne sont pas modifiables au niveau de l’arcade, ou très peu (et pas 
+										de façon récurrente).
+									</p>
+									<p>
+										Concernant les panneaux mousse, pareil: on trouve des mousses plutôt souples et donc agréables 
+										pour le cheval en terme de sensations – mais attention à leur vieillissement! – ou des mousses 
+										trop dures, qui rigidifient les muscles attenants. Dans tous les cas, ce type de panneau est 
+										modifiable uniquement par un sellier: découdre le panneau, retailler la mousse, refabriquer une 
+										enveloppe en cuir, refabriquer et recoudre le panneau.
+									</p>
+								</div>
+							</div>
+							<div class="col-4 col-12-narrower">
+								<figure role="figure" class="image featured" aria-label="Selle et laine">
+									<img src="../assets/images/saddleWhy01.jpg" alt="Selle et laine" />
+								</figure>
+							</div>
+						</div>
+						<div class="row gtr-200">
+							<div class="col-4 col-12-narrower">
+								<figure role="figure" class="image featured" aria-label="Des cales">
+									<img src="../assets/images/saddleWhy02.jpg" alt="Des cales" />
+								</figure>
+							</div>
+							<div class="col-8 col-12-narrower">
+								<div class="content">
+									<p class="light-emphasis">
+										Au contraire, TOUS les Saddle-fitters, quelle que soit leur base de formation, vont 
+										plébisciter la selle modifiable. Pourquoi ?
+									</p>
+									<p>
+										Parce que TOUS les chevaux changent. Continuellement. Au moindre changement d’alimentation, 
+										de lieu de vie, de moral, de santé. Au moindre changement de maréchal-ferrant(e) ou de podologue 
+										qui les suit. De l’ostéopathe consulté(e). De praticien(ne) shiatsu. De l’enseignant(e). De cavalier(e).
+									</p>
+									<p>
+										Quand on se pose la question d’équiper un couple, on devrait donc - et ce quel que soit le niveau du couple - 
+										se poser la question de la pérennité de l’équipement: tant que la morphologie du cavalier et son niveau 
+										permettent de garder la même selle, pourquoi changer ? Et si on veut pouvoir garder une selle adaptée sans la 
+										changer… il faut qu’elle soit adaptable (merci l’évidence), et si possible, pour un moindre coût !
+									</p>
+								</div>
+							</div>
+						</div>
+
+
+						<div class="row gtr-200">
+							<div class="col-8 col-12-narrower">
+								<div class="content">
+									<p>
+										Trop peu de cavaliers sont sensibilisés à l’immense impact de la selle sur le corps du cheval. 
+										Les mesures au capteur de pression que j’ai pu effectuer ont achevé de me convaincre: quasiment 
+										TOUS les professionnels du monde du cheval sous-estiment l’impact d’une selle non-adaptée (le 
+										fameux « cette selle va à tous les chevaux »). Personne ne se rend compte qu’une selle ne serait-
+										ce qu’une taille d’arcade trop large, et avec des pointes d’arcade trop courtes, dégrade de façon 
+										énorme une locomotion - et cause forcément un inconfort au cheval. Personne ne parle du fait qu’un 
+										cavalier ne devrait pas se battre contre sa selle qui n’est pas équilibrée (« y’a qu’à faire de la 
+										mise en selle !»). Mais à quoi sert de combattre la gravité ? Ce n’est pas cela l’équitation.
+									</p>
+									<p>
+										C’est lors d’une simple séance de fitting que les gens ressentent ce qu’une simple cale peut changer, 
+										sur la locomotion ou même sur leur posture ! Et quand on goûte à la vraie neutralité et au vrai bon 
+										équilibrage, on ne reviendrait pour rien au monde sur cette sensation.
+									</p>
+								</div>
+							</div>
+							<div class="col-4 col-12-narrower">
+								<figure role="figure" class="image featured" aria-label="Une femme sur un cheval">
+									<img src="../assets/images/saddleWhy03.jpg" alt="Une femme sur un cheval" />
+								</figure>
+							</div>
+						</div>
+						<div class="row gtr-200">
+							<div class="col-4 col-12-narrower">
+								<figure role="figure" class="image featured" aria-label="Deux selles">
+									<img src="../assets/images/saddleWhy04.jpg" alt="Deux selles" />
+								</figure>
+							</div>
+							<div class="col-8 col-12-narrower">
+								<div class="content">
+									<p>
+										A titre d’exemple, Wintec/Bates propose 7 tailles d’arcades (hors gamme « Wide »), Thorowgood/Fairfax 
+										en propose également 7 (Gamme R-bar), Stubben/Prestige/Erreplus en offrent 9 (à la commande: une fois 
+										la selle fabriquée, les arçons sont modifiables de +2 ou -2 tailles par presse et plus par le système 
+										Treeclix). Quant aux panneaux modifiables (laine, CAIR, ou autres systèmes type Flow), leur intérêt 
+										n’est pas uniquement pour le cheval: il est aussi pour le porte-feuille du client. En effet, lorsque 
+										la selle peut-être réglée sur place par ajout/suppression de laine, on parle d’une modification à des 
+										tarifs bien moindres que celle d’un panneau mousse.
+									</p>
+									<p>
+										Sans même parler de bien-entre animal (qui est un sujet primordial), dans une époque où l’on parle de 
+										plus en plus de consommation responsable, de cycle de vie pérenne des objets et où le marché de l’occasion 
+										se développe, l’achat d’une selle adaptable est un choix économique, écologique et stratégique.
+									</p>
+									<p>
+										Pour le cavalier, choisir une enfourchure qui convient à sa morphologie est gage de confort, de sensation, 
+										de sécurité et d’efficacité.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+			<!-- Footer -->
+			<?php include("commons/footer.html"); ?>
+
+		</div>
+
+		<!-- Scripts -->
+		<?php include("commons/scripts.html"); ?>
+		<script>$('#nav-saddle-fitting').addClass('current')</script>
+	</body>
+</html>
