103fd80fdcc9b33bd70e6be3fff57be06f2964d8
Andréa <andrea.marnier@gmail.com>
2025-08-28 21:48:12 +0200

    feat: add bit fitting info page

---
+++ pages/bit-info.php
@@ -0,0 +1,102 @@
+<!DOCTYPE HTML>
+<!--
+	Arcana by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+
+	Modified by @PetitCoinCoin
+-->
+<html>
+	<head>
+		<title>Shape Bit-fitting - Informations</title>
+		<?php include("commons/meta.html"); ?>
+		<link rel="stylesheet" href="/assets/css/main.css" />
+	</head>
+
+	<body class="is-preload">
+		<div id="page-wrapper">
+			
+			<!-- Header -->
+			<?php include("commons/header.html"); ?>
+			<!-- Main -->
+				<section class="wrapper style1">
+					<div class="container">
+						<div class="row gtr-200">
+							<div class="col-8 col-12-narrower">
+								<div class="content">
+									<p>
+										En 2024, dans une logique d’évolution en conseil matériel, j’ai suivi la formation 
+										en bit-fitting proposée par Ergo Equine (Laetitia Ruzzene) afin d'également proposer 
+										un conseil en adaptation du bridon et du mors, en tant qu'activité annexe. 
+									</p>
+									<p>
+										Une sélection d’embouchures et de bridons bien conçus, logiques et pérennes pour optimiser 
+										la locomotion du cheval au travail. 
+									</p>
+									<p>
+										Je ne travaille qu’avec des mors deux anneaux libres et/ou olive pour les essais, 
+										donnant la priorité au choix des canons. Des anneaux différents peuvent ensuite être choisis 
+										à la commande selon mes observations et notre discussion.
+									</p>
+								</div>
+							</div>
+							<div class="col-4 col-12-narrower">
+								<figure role="figure" class="image featured" aria-label="Une tête de cheval">
+									<img src="../assets/images/bitInfo01.jpg" alt="Une tête de cheval" />
+								</figure>
+							</div>
+						</div>
+						<div class="row gtr-200">
+							<div class="col-4 col-12-narrower">
+								<figure role="figure" class="image featured" aria-label="Des bridons">
+									<img src="../assets/images/bitInfo02.jpg" alt="Des bridons" />
+								</figure>
+							</div>
+							<div class="col-8 col-12-narrower">
+								<div class="content">
+									<p>
+										Je ne pratique le bit-fitting que sur des couples déjà équipés de selles adaptables 
+										(sur mon conseil ou ceux de collègues travaillant avec les mêmes principes d’ergonomie, 
+										c’est à dire une selle à l’arcade adaptée au cheval).<br />
+										Je limite mon champ d’exercice au bridon, c’est à dire que je n’exerce pas en conseil 
+										pour les brides, ni les ennassures (j’ai fait le choix de ne pas investir dans du matériel 
+										d’essai pour ces spécificités).
+									</p>
+								</div>
+							</div>
+						</div>
+						<h3>Contenu d'une séance</h3>
+						<p><ul>
+							<li>Anamnèse du couple (passif du cheval, du cavalier, etc)</li>
+							<li>Observation de la morphologie du cheval (tête, bouche et nuque en détail + conformation générale)</li>
+							<li>Mesures de la tête du cheval</li>
+							<li>Observation de la locomotion du cheval sans cavalier</li>
+							<li>Observation et discussion autour du matériel employé actuellement</li>
+							<li>Analyse du matériel actuel en dynamique (monté)</li>
+							<li>Essais d’embouchures et de têtières pour l’optimisation de la locomotion du cheval</li>
+						</ul></p>
+						<h3>Informations à connaitre avant la séance</h3>
+						<p>
+							Le cheval doit être pansé et prêt à seller pour l’heure du RDV, mais ni bridé, ni sellé ni échauffé au préalable.
+						</p>
+						<p>
+							Vous devez être en tenue d’équitation et monter à cheval pendant la séance.<br/>
+							Séance “pré-débourrage” possible, tarifs et durée moindres.
+						</p>
+						<p>
+							En amont de la séance, je vous enverrai un questionnaire à remplir afin de cerner votre besoin: 
+							votre couple, votre pratique et votre budget.
+						</p>
+					</div>
+				</section>
+
+			<!-- Footer -->
+			<?php include("commons/footer.html"); ?>
+
+		</div>
+
+		<!-- Scripts -->
+		<?php include("commons/scripts.html"); ?>
+		<script>$('#nav-bit-fitting').addClass('current')</script>
+	</body>
+</html>
