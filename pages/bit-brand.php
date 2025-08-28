3f25d88191d596f2bafbb099016808575623f54d
Andréa <andrea.marnier@gmail.com>
2025-08-28 22:32:12 +0200

    feat: add bit brands page

---
+++ pages/bit-brand.php
@@ -0,0 +1,122 @@
+<!DOCTYPE HTML>
+<!--
+	Arcana by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+
+	Modified by @PetitCoinCoin
+-->
+<html>
+	<head>
+		<title>Shape Bit-fitting - Marques</title>
+		<?php include("commons/meta.html"); ?>
+		<link rel="stylesheet" href="/assets/css/main.css" />
+	</head>
+
+	<body class="is-preload">	
+		<div id="page-wrapper">
+			
+			<!-- Header -->
+			<?php include("commons/header.html"); ?>
+			<!-- Main -->
+				<section class="wrapper style1">
+					<div class="container">
+						<div class="content">
+							<h3>Marques</h3>
+							<h6>NEUE SHUHE</h6>
+							<div class="row gtr-200">
+								<div class="col-9 col-12-narrower">
+									<div class="content">
+										<p>
+											Des designs de mors aux cintrages et sections extrêmement travaillés, pour optimiser 
+											la locomotion de toutes les morphologies de chevaux, et un alliage breveté qui chauffe 
+											rapidement pour un contact agréable en bouche. Toutes les formes de canons sont 
+											combinables avec des anneaux divers à la commande. 
+										</p>
+									</div>
+								</div>
+								<div class="col-3 col-12-narrower">
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/bitBrandNeueShuhe.jpg" alt="" />
+									</figure>
+								</div>
+							</div>
+							<h6>BOMBERS</h6>
+							<div class="row gtr-200">
+								<div class="col-9 col-12-narrower">
+									<div class="content">
+										<p>
+											Deux matières au choix (Sweet Iron ou Titane) pour varier diamètre et poids de l’embouchure, 
+											des canons au cintrage travaillé.
+										</p>
+									</div>
+								</div>
+								<div class="col-3 col-12-narrower">
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/bitBrandBombers.jpg" alt="" />
+									</figure>
+								</div>
+							</div>
+							<h6>BRIDLE2FIT</h6>
+							<div class="row gtr-200">
+								<div class="col-9 col-12-narrower">
+									<div class="content">
+										<p>
+											Des pièces au détail permettant de composer des bridons sur-mesure, avec des têtières, 
+											muserolles, frontaux et montants de tailles et formes diverses.
+										</p>
+									</div>
+								</div>
+								<div class="col-3 col-12-narrower">
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/bitBrandBridle2Fit.jpg" alt="" />
+									</figure>
+								</div>
+							</div>
+							<div class="row gtr-200">
+								<div class="col-9 col-12-narrower">
+									<div class="content">
+										<p>
+											Pour les personnes ayant un budget plus élevé, je vous orienterais 
+											vers Bluemane sellier qui pourra vous confectionner le bridon parfait 
+											et personnalisé !
+										</p>
+									</div>
+								</div>
+								<div class="col-3 col-12-narrower">
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/bitBrandBluemane.jpg" alt="" />
+									</figure>
+								</div>
+							</div>
+							<h6>STUBBEN</h6>
+							<div class="row gtr-200">
+								<div class="col-9 col-12-narrower">
+									<div class="content">
+										<p>
+											Les Bridons Freedom II, Leitrim et ProJump font partie de mon trousseau d’essai, 
+											et conviennent aux chevaux ayant des proportions de tête assez standard ou au contraire 
+											une nuque très particulière.
+										</p>
+									</div>
+								</div>
+								<div class="col-3 col-12-narrower">
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/bitBrandStubben.jpg" alt="" />
+									</figure>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+			<!-- Footer -->
+			<?php include("commons/footer.html"); ?>
+
+		</div>
+
+		<!-- Scripts -->
+		<?php include("commons/scripts.html"); ?>
+		<script>$('#nav-bit-fitting').addClass('current')</script>
+	</body>
+</html>
