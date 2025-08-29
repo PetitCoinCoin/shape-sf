de91fbeeb89bdb20ea8dc4e2cb2e2d4fbac9d292
Andréa <andrea.marnier@gmail.com>
2025-08-29 19:11:24 +0200

    feat: add saddle brands page

---
+++ pages/saddle-brand.php
@@ -0,0 +1,289 @@
+<!DOCTYPE HTML>
+<!--
+	Arcana by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+
+	Modified by @PetitCoinCoin
+-->
+<html>
+	<head>
+		<title>Shape Saddle-fitting - Marques</title>
+		<?php include("commons/meta.html"); ?>
+		<link rel="stylesheet" href="/assets/css/main.css" />
+	</head>
+
+	<body class="is-preload">	
+		<div id="page-wrapper">
+			
+			<!-- Header -->
+			<?php include("commons/header.html"); ?>
+			<!-- Main -->
+				<section class="wrapper style1">
+					<div class="container">
+						<div class="content">
+							<h3>Marques</h3>
+							<h6>WINTEC/BATES</h6>
+							<div class="row gtr-200">
+								<div class="col-6 col-12-narrower">
+									<div class="content">
+										<p>
+											Un rapport qualité/prix imbattable pour ces selles à l’adaptabilité inégalée 
+											(arcades interchangeables et inserts directement installés dans les panneaux). 
+											Leur durabilité n’est plus à prouver, notamment grâce à l’absence d’entretien 
+											requis des panneaux CAIR.
+										</p>
+										<p>
+											Synthétique nouvelle génération (Wintec) ou cuir (Bates), certains modèles existent 
+											en deux matières, d’autres sont réservés à une seule marque. Modèles mixtes, obstacle, 
+											dressage et endurance, il y en a pour tous les besoins côté cavalier, mais en restant 
+											dans du prêt-à-porter (pas de quartiers plus longs/plus projetés disponibles). 
+										</p>
+									</div>
+								</div>
+								<div class="col-6 col-12-narrower flex-h2">
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/saddleBrandWintecBates01.jpg" alt="" />
+									</figure>
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/saddleBrandWintecBates02.jpg" alt="" />
+									</figure>
+								</div>
+							</div>
+							<h6>THOROWGOOD</h6>
+							<div class="row gtr-200">
+								<div class="col-9 col-12-narrower">
+									<div class="content">
+										<p>
+											Une conception vraiment idéale pour certaines morphologies de chevaux avec des pointes 
+											d’arcades longues – et des arcades interchangeables. Pour le cavalier, des sièges très 
+											différents de Wintec: la complémentarité de ces deux marques est un atout pour seller 
+											les couples à petit budget.
+										</p>
+										<p>
+											Thorowgood propose une gamme 100% synthétique (gamme T4, siège en suédine), un hybride 
+											synthétique/cuir (gamme T8) et une gamme 100% cuir (Kent&Master’s).
+										</p>
+									</div>
+								</div>
+								<div class="col-3 col-12-narrower">
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/saddleBrandThorowgood.jpg" alt="" />
+									</figure>
+								</div>
+							</div>
+							<h6>FAIRFAX</h6>
+							<div class="row gtr-200">
+								<div class="col-6 col-12-narrower">
+									<div class="content">
+										<p>
+											Côté cheval, une conception identique à Thorowgood, mais des sièges aux caractéristiques 
+											différentes pour le cavalier. Selles d’obstacles, vraies selles mixtes ou de dressage, 
+											double ou mono quartier, pour un budget compris entre 2500 et 3400€.
+										</p>
+									</div>
+								</div>
+								<div class="col-6 col-12-narrower flex-h2">
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/saddleBrandFairfax01.jpg" alt="" />
+									</figure>
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/saddleBrandFairfax02.jpg" alt="" />
+									</figure>
+								</div>
+							</div>
+							<h6>IKONIC</h6>
+							<div class="row gtr-200">
+								<div class="col-6 col-12-narrower">
+									<div class="content">
+										<p>
+											Cette marque belge produit des selles munies d’arcades interchangeables, en cuir ou 
+											en version hybride cuir/synthétique, pour des budget démarrant à 1550€ en version hybride.
+										</p>
+										<p>
+											Avec des options en permanente évolution sur ces modèles, la marque propose une gamme très 
+											évolutive, qui peut convenir à certaines morphologies de cavalier, et aux chevaux qui apprécient 
+											ces arçons rigides et droits. Une petite personnalisation est possible sur tous les modèles.
+										</p>
+									</div>
+								</div>
+								<div class="col-6 col-12-narrower flex-h3">
+										<figure role="figure" class="image featured">
+											<img src="../assets/images/saddleBrandIkonic01.jpg" alt="" />
+										</figure>
+										<figure role="figure" class="image featured">
+											<img src="../assets/images/saddleBrandIkonic02.jpg" alt="" />
+										</figure>
+										<figure role="figure" class="image featured">
+											<img src="../assets/images/saddleBrandIkonic03.jpg" alt="" />
+										</figure>
+								</div>
+							</div>
+							<h6>STUBBEN</h6>
+							<div class="row gtr-200">
+								<div class="col-6 col-12-narrower">
+									<div class="content">
+										<p>
+											Des arçons à la composition brevetée, toniques, travaillés différemment pour chaque modèle, 
+											qui transmettent au cavalier des sensations bien spécifiques. Une fois essayé, chaque cavalier 
+											se rendra compte de la différence de conception par rapport aux autres marques.
+										</p>
+									</div>
+								</div>
+								<div class="col-6 col-12-narrower flex-h2">
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/saddleBrandStubben01.jpg" alt="" />
+									</figure>
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/saddleBrandStubben02.jpg" alt="" />
+									</figure>
+								</div>
+							</div>
+							<div class="row gtr-200">
+								<div class="col-9 col-12-narrower">
+									<div class="content">
+										<p>
+											Avec 9 tailles d’arcades disponibles à la commande (selles adaptables par 
+											presse de +2 ou -2 tailles tout au long de leur vie) et le système TREECLIX 
+											posé sur la selle qui permet de modifier la selle à l’écurie, des options 
+											d’adaptation tant au cavalier qu'au cheval multiples, les selles Stubben 
+											sont un vrai outil de travail pour le saddle-fitter, et un investissement 
+											durable pour le couple cheval/cavalier.
+										</p>
+									</div>
+								</div>
+								<div class="col-3 col-12-narrower">
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/saddleBrandStubben03.jpg" alt="" />
+									</figure>
+								</div>
+							</div>
+							<div class="row gtr-200">
+								<div class="col-6 col-12-narrower">
+									<div class="content">
+										<p>
+											Des sièges très différents pour les 8 modèles de dressage, 7 modèles 
+											d’obstacle et 4 modèles mixtes de la gamme, et des options de personnalisations 
+											esthétiques possibles si vous voulez créer “VOTRE” selle unique.
+										</p>
+									</div>
+								</div>
+								<div class="col-6 col-12-narrower flex-h2">
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/saddleBrandStubben04.jpg" alt="" />
+									</figure>
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/saddleBrandStubben05.jpg" alt="" />
+									</figure>
+								</div>
+							</div>
+							<h6>AUTRES MARQUES</h6>
+							<p>
+								Au hasard des clients, je prends en dépôt vente des selles d’autres marques si elles sont modifiables, 
+								de bonne conception et en bon état structurel.<br />
+								Pas de dépôt-vente de selle non modifiables, me contacter selon le modèle que vous pourriez me confier.
+							</p>
+							<h6>ACCESSOIRES DE LA SELLE</h6>
+							<div class="row gtr-200">
+								<div class="col-6 col-12-narrower">
+									<div class="content">
+										<p class="light-emphasis">
+											Tapis & Correcteurs / Amortisseurs 
+										</p>
+										<p>
+											J’ai choisi les produits Mattes pour leur découpe intelligente, la facilité d’utilisation 
+											des produits (permettant de rendre les cavaliers autonomes dans leurs réglages), la qualité 
+											de conception et le choix de tailles s’adaptant à tous les équidés. En bonus, une marque 
+											qui offre des possibilités de personnalisations indénombrables.
+										</p>
+									</div>
+								</div>
+								<div class="col-6 col-12-narrower flex-h2">
+									<div class="flex-v">
+										<figure role="figure" class="image featured">
+											<img src="../assets/images/saddleBrandMattes01.jpg" alt="" />
+										</figure>
+										<figure role="figure" class="image featured">
+											<img src="../assets/images/saddleBrandMattes02.jpg" alt="" />
+										</figure>
+									</div>
+									<div class="flex-v">
+										<figure role="figure" class="image featured">
+											<img src="../assets/images/saddleBrandMattes03.jpg" alt="" />
+										</figure>
+										<figure role="figure" class="image featured">
+											<img src="../assets/images/saddleBrandMattes04.jpg" alt="" />
+										</figure>
+									</div>
+								</div>
+							</div>
+							<div class="row gtr-200">
+								<div class="col-9 col-12-narrower">
+									<div class="content">
+										<p class="light-emphasis">
+											Amortisseurs 
+										</p>
+										<p>
+											Selon les besoins du couple, je peux vous proposer au cours de la séance des marques 
+											diverses en terme d’amortisseurs (Thinline, Amortisseur Orange). Pour certains, je ne 
+											suis pas revendeuse, donc je vous indiquerais les caractéristiques afin que vous 
+											commandiez par vous-même.
+										</p>
+									</div>
+								</div>
+								<div class="col-3 col-12-narrower">
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/saddleBrandOrange.jpg" alt="" />
+									</figure>
+								</div>
+							</div>
+							<div class="row gtr-200">
+								<div class="col-6 col-12-narrower">
+									<div class="content">
+										<p class="light-emphasis">
+											Sangles
+										</p>
+										<p>
+											Une sélection de formes et de conception choisies chez Stubben, Mattes, Picasso, 
+											Thorowgood et Kieffer. 
+										</p>
+									</div>
+								</div>
+								<div class="col-6 col-12-narrower flex-h2">
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/saddleBrandSangles01.jpg" alt="" />
+									</figure>
+									<figure role="figure" class="image featured">
+										<img src="../assets/images/saddleBrandSangles02.jpg" alt="" />
+									</figure>
+								</div>
+							</div>
+							<div class="row gtr-200">
+								<div class="col-12 col-12-narrower">
+									<div class="content">
+										<p class="light-emphasis">
+											Étriers
+										</p>
+										<p>
+											L’importance d’un plancher adapté à vos préférences motrices est fondamental pour avoir 
+											vos appuis stables, et peu nombreux sont les humains à avoir absolument besoin de planchers 
+											inclinés et amortis… En fonction de vos préférences vues en séance, je pourrais vous conseiller 
+											tel ou tel design de marques différentes.
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+			<!-- Footer -->
+			<?php include("commons/footer.html"); ?>
+
+		</div>
+
+		<!-- Scripts -->
+		<?php include("commons/scripts.html"); ?>
+		<script>$('#nav-saddle-fitting').addClass('current')</script>
+	</body>
+</html>
